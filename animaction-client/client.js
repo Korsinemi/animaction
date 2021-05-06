affd70820571be9b829977be4e11a8fa366e9d15
KitsuneCode <im.kitsunecode@gmail.com>
2021-05-06 15:47:10 -0500

    uwu

--- animaction-client/client.js
+++ animaction-client/client.js
@@ -1,10 +1,21 @@
-const dance = require("../gifs/dance.json");
-const wag = require("../gifs/wag.json");
+/* Creado por KitsuneCode, la copia sera denunciada u.u */
+// GIFS -w-
+const dance = require("../functions/gifs/dance.json");
+const wag = require("../functions/gifs/wag.json");
+// TEXTO .w.
+const answers = require("../functions/text/answers.json");
+// IMAGENES owo
+const anime = require("../functions/images/anime.json")
 
 const animaction = () => {
   return {
+    // GIFS -w-
     dance: () => dance[mathRandom(anime.length)],
     wag: () => wag[mathRandom(wag.length)],
+    // TEXTO .w.
+    answers: () => answers[mathRandom(answers.length)],
+    // IMAGENES owo
+    anime: () => anime[mathRandom(anime.length)],
   };
 };
 
