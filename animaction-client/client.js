eb35488339919df1c0407987b0d6d2c9fcdbc2ac
Kira Itakura <im.kitsunecode@gmail.com>
2021-05-07 14:21:12 -0500

    Actualizado .w.

--- animaction-client/client.js
+++ animaction-client/client.js
@@ -2,6 +2,7 @@
 // GIFS -w-
 const dance = require("../functions/gifs/dance.json");
 const wag = require("../functions/gifs/wag.json");
+const sip = require("../functions/gifs/sip.json")
 // TEXTO .w.
 const answers = require("../functions/text/answers.json");
 // IMAGENES owo
@@ -10,8 +11,9 @@ const anime = require("../functions/images/anime.json")
 const animaction = () => {
   return {
     // GIFS -w-
-    dance: () => dance[mathRandom(anime.length)],
+    dance: () => dance[mathRandom(dance.length)],
     wag: () => wag[mathRandom(wag.length)],
+    sip: () => sip[mathRandom(sip.length)],
     // TEXTO .w.
     answers: () => answers[mathRandom(answers.length)],
     // IMAGENES owo
